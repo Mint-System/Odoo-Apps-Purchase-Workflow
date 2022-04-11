f798a7d536b06ccbf62969edf58a6fb58f76db5d
janikvonrotz <contact@janikvonrotz.ch>
2022-04-11 14:33:12 +0200

    update readmes

--- purchase_order_line_position/__manifest__.py
+++ purchase_order_line_position/__manifest__.py
@@ -1,24 +1,17 @@
 {
-    'name': "Purchase Order Line Position",
-
-    'summary': """
+    "name": "Purchase Order Line Position",
+    "summary": """
         Use purchase order line position for linked delivery orders and outgoing invoices.
     """,
-    
-    'author': 'Mint System GmbH, Odoo Community Association (OCA)',
-    'website': 'https://www.mint-system.ch',
-    'category': 'Purchase',
-    'version': '14.0.1.1.0',
-    'license': 'AGPL-3',
-    
-    'depends': ['purchase'],
-
-    'data': [
-        'views/views.xml',
-    ],
-
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Purchase",
+    "version": "14.0.1.1.0",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
+    "data": ["views/views.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
     "images": ["images/screen.png"],
 }
--- purchase_order_line_price_default/__manifest__.py
+++ purchase_order_line_price_default/__manifest__.py
@@ -1,20 +1,16 @@
 {
-    'name': "Purchase Order Line Price Default",
-
-    'summary': """
+    "name": "Purchase Order Line Price Default",
+    "summary": """
         Set price to zero if seller is not available.
     """,
-    
-    'author': 'Mint System GmbH, Odoo Community Association (OCA)',
-    'website': 'https://www.mint-system.ch',
-    'category': 'Purchse',
-    'version': '14.0.1.1.0',
-    'license': 'AGPL-3',
-    
-    'depends': ['purchase'],
-
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Purchse",
+    "version": "14.0.1.1.0",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
     "images": ["images/screen.png"],
 }
