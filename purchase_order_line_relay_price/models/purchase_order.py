bea2099e39ca0a41d6487a620f470ae527495860
janikvonrotz <contact@janikvonrotz.ch>
2022-04-27 17:31:05 +0200

    [purchase_order_line_relay_price] fix if no seller found

--- purchase_order_line_relay_price/models/purchase_order.py
+++ purchase_order_line_relay_price/models/purchase_order.py
@@ -16,24 +16,24 @@ class PurchaseOrder(models.Model):
             if line.product_id.seller_ids:
                 product_id = line.product_id
                 seller_ids = line.product_id.seller_ids.filtered(lambda l: l.min_qty >= line.product_qty and l.price > 0)
-                
-                # Calculate minimal price
-                min_price = min(seller_ids.mapped(lambda l: l.min_qty * l.price))
-                min_seller_ids = seller_ids.filtered(lambda l: l.min_qty * l.price == min_price)
-                if line.price_subtotal > min_price:
-                    
-                    # Generate not with lower prices
-                    summary = _('Check lower relay price')
-                    note = _('<p>For the product "%s" a lower relay price has been found:</p>') % product_id.display_name
-                    note += "<ul>"
-                    for seller in min_seller_ids:
-                        note += _('<li>%s %s for %s with delivery time of %s days.</li>') % (seller.min_qty, product_id.uom_id.name, seller.price, seller.delay)
-                    note += "</ul>"
+                if seller_ids:
+                    # Calculate minimal price
+                    min_price = min(seller_ids.mapped(lambda l: l.min_qty * l.price))
+                    min_seller_ids = seller_ids.filtered(lambda l: l.min_qty * l.price == min_price)
+                    if line.price_subtotal > min_price:
+                        
+                        # Generate not with lower prices
+                        summary = _('Check lower relay price')
+                        note = _('<p>For the product "%s" a lower relay price has been found:</p>') % product_id.display_name
+                        note += "<ul>"
+                        for seller in min_seller_ids:
+                            note += _('<li>%s %s for %s with delivery time of %s days.</li>') % (seller.min_qty, product_id.uom_id.name, seller.price, seller.delay)
+                        note += "</ul>"
 
-                    # Create activity to check prices
-                    line.order_id.activity_schedule(
-                        activity_type_id=self.env.ref('mail.mail_activity_data_todo').id,
-                        summary=summary,
-                        note=note,
-                        user_id=line.order_id.user_id.id
-                    )
+                        # Create activity to check prices
+                        line.order_id.activity_schedule(
+                            activity_type_id=self.env.ref('mail.mail_activity_data_todo').id,
+                            summary=summary,
+                            note=note,
+                            user_id=line.order_id.user_id.id
+                        )
