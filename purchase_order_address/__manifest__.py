fc501590b8e610290f4122c586e7832109dbf4a6
janikvonrotz <contact@janikvonrotz.ch>
2022-05-19 09:42:30 +0200

    [purchase_order_address] update translation

--- purchase_order_address/__manifest__.py
+++ purchase_order_address/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Purchase",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "license": "AGPL-3",
     "depends": ["purchase", "partner_type_order"],
     "data": ["views/purchase_order_form.xml", "data/mail_template_data.xml"],
