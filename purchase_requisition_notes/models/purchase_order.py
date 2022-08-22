bcf100ee92bab1826934e806bc027b8ed87533e6
janikvonrotz <contact@janikvonrotz.ch>
2022-08-22 10:09:43 +0200

    [purchase_requisition_notes] onchange defaults

--- purchase_requisition_notes/models/purchase_order.py
+++ purchase_requisition_notes/models/purchase_order.py
@@ -8,10 +8,12 @@ class PurchaseOrder(models.Model):
 
     @api.onchange('requisition_id')
     def _onchange_requisition_id(self):
-        super(PurchaseOrder, self)._onchange_requisition_id()
+        res = super(PurchaseOrder, self)._onchange_requisition_id()
         
         if self.requisition_id and (not self.note_header or self.note_header == '<p><br></p>'):
             self.note_header = self.requisition_id.note_header
 
         if self.requisition_id and (not self.note_footer or self.note_footer == '<p><br></p>'):
             self.note_footer = self.requisition_id.note_footer
+        
+        return res
