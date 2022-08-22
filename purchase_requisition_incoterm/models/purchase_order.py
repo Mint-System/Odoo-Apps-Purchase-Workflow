38bc9cad1fbc8d7fb7884d809e432ef7a6bef216
janikvonrotz <contact@janikvonrotz.ch>
2022-08-22 10:08:53 +0200

    [purchase_requisition_incoterm] new defaults

--- purchase_requisition_incoterm/models/purchase_order.py
+++ purchase_requisition_incoterm/models/purchase_order.py
@@ -8,7 +8,8 @@ class PurchaseOrder(models.Model):
 
     @api.onchange('requisition_id')
     def _onchange_requisition_id(self):
+        """Always override incoterm from purchase contract."""
         res = super(PurchaseOrder, self)._onchange_requisition_id()
-        if not self.incoterm_id and self.requisition_id:
+        if not self.incoterm_id and self.requisition_id.incoterm_id:
             self.incoterm_id = self.requisition_id.incoterm_id
         return res
