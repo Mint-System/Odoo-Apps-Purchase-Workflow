ce9f31cd633b10a30cbcffa35ac1b504170523a1
janikvonrotz <contact@janikvonrotz.ch>
2022-12-12 09:41:31 +0100

    [purchase_order_comment] make copy comment optional

--- purchase_order_comment/models/purchase_order.py
+++ purchase_order_comment/models/purchase_order.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+import ast
 
 
 class PurchaseOrder(models.Model):
@@ -9,6 +10,7 @@ class PurchaseOrder(models.Model):
     @api.onchange('requisition_id')
     def _onchange_requisition_id(self):
         res = super(PurchaseOrder, self)._onchange_requisition_id()
-        if self.requisition_id and not self.comment:
+        copy_requisition_comment = ast.literal_eval(self.env['ir.config_parameter'].sudo().get_param('purchase.order.copy_requisition_comment', "False"))
+        if self.requisition_id and not self.comment and copy_requisition_comment:
             self.comment = self.requisition_id.comment
         return res
