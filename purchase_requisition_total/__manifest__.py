d8d98580aca5d06fe16c7862ef46acf637b59c3f
janikvonrotz <contact@janikvonrotz.ch>
2022-12-13 17:10:19 +0100

    [purchase_requisition_total] set version

--- purchase_requisition_total/__manifest__.py
+++ purchase_requisition_total/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Purchase",
-    "version": "14.0.1.2.1",
+    "version": "14.0.1.3.0",
     "license": "AGPL-3",
     "depends": ["purchase_requisition"],
     "data": ["views/purchase_requisition.xml"],
