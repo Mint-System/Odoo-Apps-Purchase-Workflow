6ac30be943ffb13b12234f4074fbf816eac7ea74
janikvonrotz <contact@janikvonrotz.ch>
2022-12-29 16:11:57 +0100

    [purchase_requisition_reference] make optional

--- purchase_order_comment/models/purchase_order.py
+++ purchase_order_comment/models/purchase_order.py
@@ -10,7 +10,7 @@ class PurchaseOrder(models.Model):
     @api.onchange('requisition_id')
     def _onchange_requisition_id(self):
         res = super(PurchaseOrder, self)._onchange_requisition_id()
-        copy_requisition_comment = ast.literal_eval(self.env['ir.config_parameter'].sudo().get_param('purchase.order.copy_requisition_comment', "False"))
+        copy_requisition_comment = ast.literal_eval(self.env['ir.config_parameter'].sudo().get_param('purchase.order.copy_requisition_comment', 'False'))
         if self.requisition_id and not self.comment and copy_requisition_comment:
             self.comment = self.requisition_id.comment
         return res
--- purchase_requisition_reference/models/purchase_order.py
+++ purchase_requisition_reference/models/purchase_order.py
@@ -1,6 +1,7 @@
 from odoo import _, api, fields, models
 import logging
 _logger = logging.getLogger(__name__)
+import ast
 
 
 class PurchaseOrder(models.Model):
@@ -9,6 +10,7 @@ class PurchaseOrder(models.Model):
     @api.onchange('requisition_id')
     def _onchange_requisition_id(self):
         res = super(PurchaseOrder, self)._onchange_requisition_id()
-        if not self.partner_ref and self.requisition_id:
+        copy_reference = ast.literal_eval(self.env['ir.config_parameter'].sudo().get_param('purchase_requisition_reference.copy_reference', 'False'))
+        if not self.partner_ref and self.requisition_id and copy_reference:
             self.partner_ref = self.requisition_id.partner_ref
         return res
