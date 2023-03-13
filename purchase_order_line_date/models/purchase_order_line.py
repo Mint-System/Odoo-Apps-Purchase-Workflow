feb1f2ebfc505fb55ead544fd7ad0465734149fd
Mitchel Admin <mitcheladmin@example.com>
2023-03-13 14:36:17 +0100

    remove logger statements

--- purchase_order_line_date/models/purchase_order_line.py
+++ purchase_order_line_date/models/purchase_order_line.py
@@ -12,18 +12,15 @@ class PurchaseOrderLine(models.Model):
         for move in res:
             move["date"] = self.date_planned + relativedelta(days=self.company_id.po_lead)
             move["date_deadline"] = self.date_planned
-        _logger.warning(res)
         return res
 
     def write(self, values):
         """When date planned is updated update move dates."""
-        _logger.warning('write')
         if values.get('date_planned'):
             self.move_ids.date = fields.Datetime.to_datetime(values.get('date_planned')) + relativedelta(days=self.company_id.po_lead)
         return super(PurchaseOrderLine, self).write(values)
 
     def _update_move_date_deadline(self, new_date):
         """Remove security lead time from date deadline."""
-        _logger.warning('_update_move_date_deadline')
         new_date = new_date - relativedelta(days=self.company_id.po_lead)
         super()._update_move_date_deadline(new_date)
