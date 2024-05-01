26949a27feac653064ee1800409610f2777e5b80
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-05-01 15:23:50 +0200

    feat: upgrade purchase_requisition_incoterm

---
+++ purchase_requisition_incoterm/models/purchase_requisition.py
@@ -0,0 +1,13 @@
+from odoo import _, api, fields, models
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class PurchaseRequisition(models.Model):
+    _inherit = "purchase.requisition"
+    
+    incoterm_id = fields.Many2one('account.incoterms', string='Incoterm')
+
+    @api.onchange('vendor_id')
+    def _onchange_incoterm_id(self):
+        self.incoterm_id = self.vendor_id.purchase_incoterm_id
