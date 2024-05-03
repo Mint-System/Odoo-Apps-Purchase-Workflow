f27423b6e8286bc352b080fc9c59a937fe838b33
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-05-03 17:18:16 +0200

    feat: upgrade purchase_requisition_notes

---
+++ purchase_requisition_notes/models/purchase_order.py
@@ -0,0 +1,19 @@
+from odoo import _, api, fields, models
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.onchange('requisition_id')
+    def _onchange_requisition_id(self):
+        res = super(PurchaseOrder, self)._onchange_requisition_id()
+        
+        if self.requisition_id and (not self.note_header or self.note_header == '<p><br></p>'):
+            self.note_header = self.requisition_id.note_header
+
+        if self.requisition_id and (not self.note_footer or self.note_footer == '<p><br></p>'):
+            self.note_footer = self.requisition_id.note_footer
+        
+        return res
