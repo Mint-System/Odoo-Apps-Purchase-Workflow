4598c4476df7f0c01b27c1293522ab7ff5726308
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-07-08 17:03:40 +0200

    feat: upgrade module, add test instructions

---
+++ purchase_requisition_payment_term/models/purchase_requisition.py
@@ -0,0 +1,27 @@
+import logging
+
+from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class PurchaseRequisition(models.Model):
+    _inherit = "purchase.requisition"
+
+    payment_term_id = fields.Many2one(
+        "account.payment.term",
+        string="Payment Terms",
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
+        compute="_compute_payment_term_id",
+        store=True,
+    )
+
+    @api.depends("vendor_id")
+    def _compute_payment_term_id(self):
+        for requisition in self:
+            if requisition.vendor_id:
+                requisition.payment_term_id = (
+                    requisition.vendor_id.property_supplier_payment_term_id
+                )
+            else:
+                requisition.payment_term_id = False
