587d3b4f14065ca11f25b0bdf590a1961090e130
Laszlo Krajnikovszkij <laszlo@laszlo.is>
2024-07-10 13:53:17 +0200

    chore: lint module, add return statement

--- purchase_requisition_order_address/models/purchase_order.py
+++ purchase_requisition_order_address/models/purchase_order.py
@@ -1,6 +1,6 @@
 import logging
 
-from odoo import api, fields, models
+from odoo import models
 
 _logger = logging.getLogger(__name__)
 
@@ -8,22 +8,9 @@ _logger = logging.getLogger(__name__)
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    partner_order_id = fields.Many2one(
-        "res.partner",
-        string="Order Address",
-        compute="_compute_partner_order_id",
-        store=True,
-        readonly=False,
-        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
-    )
-
-    @api.depends("partner_id", "requisition_id")
     def _compute_partner_order_id(self):
+        super(PurchaseOrder, self)._compute_partner_order_id()
         for order in self:
             if order.requisition_id and order.requisition_id.partner_order_id:
                 order.partner_order_id = order.requisition_id.partner_order_id
-            elif order.partner_id:
-                addr = order.partner_id.address_get(["order"])
-                order.partner_order_id = addr["order"]
-            else:
-                order.partner_order_id = False
+        return
