e689efcb92952944b563e208419ac941baddc92b
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-11-08 18:24:02 +0100

    feat: migrate module

---
+++ purchase_order_line_date/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Purchase Order Line Date",
+    "summary": """
+        This module ensure that line order date are propagated to stock pickings.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Manufacturing",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["purchase_stock"],
+    "installable": True,
+    "application": False,
+    "images": ["images/screen.png"],
+}
