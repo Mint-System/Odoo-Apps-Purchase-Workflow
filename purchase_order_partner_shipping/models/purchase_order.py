dbf287beda112304dc6407357e942020fbfdb4d3
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-11-08 19:05:48 +0100

    feat: update model

--- purchase_order_partner_shipping/models/purchase_order.py
+++ purchase_order_partner_shipping/models/purchase_order.py
@@ -4,23 +4,24 @@ from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
+
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
     partner_shipping_id = fields.Many2one(
-        'res.partner',
-        string='Delivery Address',
+        "res.partner",
+        string="Delivery Address",
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
-        compute='_compute_partner_shipping_id',
+        compute="_compute_partner_shipping_id",
         store=True,
-        readonly=True
+        readonly=True,
     )
 
-    @api.depends('partner_id')
+    @api.depends("partner_id")
     def _compute_partner_shipping_id(self):
         for order in self:
             if order.partner_id:
-                address = order.partner_id.address_get(['delivery'])
-                order.partner_shipping_id = address.get('delivery', order.partner_id.id)
+                address = order.partner_id.address_get(["delivery"])
+                order.partner_shipping_id = address.get("delivery", order.partner_id.id)
             else:
                 order.partner_shipping_id = False
