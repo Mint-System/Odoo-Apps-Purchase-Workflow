e834a475f54509223eb39c89709c47fc372b2ee4
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-11-11 10:49:16 +0100

    feat: migrate dependencies

---
+++ purchase_requisition_order_address/models/purchase_order.py
@@ -0,0 +1,16 @@
+import logging
+
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    def _compute_partner_order_id(self):
+        super(PurchaseOrder, self)._compute_partner_order_id()
+        for order in self:
+            if order.requisition_id and order.requisition_id.partner_order_id:
+                order.partner_order_id = order.requisition_id.partner_order_id
+        return
