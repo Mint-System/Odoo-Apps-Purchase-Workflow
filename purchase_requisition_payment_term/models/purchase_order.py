70381eb448cd7008e28cb882f8aa80a05ce7f7d2
Ulrich Kilian <uk@science-and-more.de>
2025-07-18 13:26:38 +0200

    fix(purchase_requisition_payment_term): correct value for purchase order triggered from blanket order

--- purchase_requisition_payment_term/models/purchase_order.py
+++ purchase_requisition_payment_term/models/purchase_order.py
@@ -8,17 +8,20 @@ _logger = logging.getLogger(__name__)
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    payment_term_id = fields.Many2one(
-        "account.payment.term",
-        string="Payment Terms",
-        compute="_compute_payment_term_id",
-        store=True,
-        readonly=False,
-        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
-    )
+    
+    payment_term_id = fields.Many2one('account.payment.term', 'Payment Terms', compute="_compute_payment_term_id", store=True, domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
 
+    @api.onchange('partner_id', 'company_id')
+    def onchange_partner_id(self):
+        res = super().onchange_partner_id()
+        if self.requisition_id and self.requisition_id.payment_term_id:
+            self.payment_term_id = self.requisition_id.payment_term_id
+        return res
+        
+    
     @api.depends("requisition_id", "partner_id", "company_id")
     def _compute_payment_term_id(self):
+        _logger.info("############### compute payment called")
         for order in self:
             if order.requisition_id and order.requisition_id.payment_term_id:
                 order.payment_term_id = order.requisition_id.payment_term_id
@@ -30,3 +33,7 @@ class PurchaseOrder(models.Model):
                 )
             else:
                 order.payment_term_id = False
+
+    
+
+    
