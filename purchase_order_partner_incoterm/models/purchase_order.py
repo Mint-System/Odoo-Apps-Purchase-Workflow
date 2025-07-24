7d852c2877c16b218abf7c02bd975d5a2afebbf2
Ulrich Kilian <uk@science-and-more.de>
2025-07-24 15:20:05 +0200

    fix(purchase_order_partner_incoterm): fix onchange method

--- purchase_order_partner_incoterm/models/purchase_order.py
+++ purchase_order_partner_incoterm/models/purchase_order.py
@@ -9,7 +9,6 @@ class PurchaseOrder(models.Model):
     @api.onchange('partner_id', 'company_id')
     def onchange_partner_id(self):
         res = super().onchange_partner_id()
-        # _logger.warning(['purchase_order_partner_incoterm', self.incoterm_id, self.partner_id.purchase_incoterm_id])
-        if not self.incoterm_id and self.partner_id.purchase_incoterm_id:
+        if self.partner_id.purchase_incoterm_id:
             self.incoterm_id = self.partner_id.purchase_incoterm_id
         return res
