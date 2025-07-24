295caa9b8ddafbb93664a30c60f8fd5d1bb29f6c
Ulrich Kilian <uk@science-and-more.de>
2025-07-24 16:51:35 +0200

    fix(purchase_order_partner_incoterm): set parner incorterm for purchase order created by purchase to order

--- purchase_order_partner_incoterm/models/purchase_order.py
+++ purchase_order_partner_incoterm/models/purchase_order.py
@@ -1,4 +1,4 @@
-from odoo import api, models
+from odoo import api, models, fields
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -12,3 +12,22 @@ class PurchaseOrder(models.Model):
         if self.partner_id.purchase_incoterm_id:
             self.incoterm_id = self.partner_id.purchase_incoterm_id
         return res
+
+    incoterm_id = fields.Many2one(
+        "account.incoterms",
+        "Incoterm", 
+        compute="_compute_incoterm_id",
+        store=True,
+        help="International Commercial Terms are a series of predefined commercial terms used in international transactions."
+        )
+
+    @api.depends("partner_id")
+    def _compute_incoterm_id(self):
+        for order in self:
+            _logger.info("######### _compute_incoterm_id called: %s", order.partner_id.purchase_incoterm_id)
+            if order.partner_id and order.partner_id.purchase_incoterm_id:
+                order.incoterm_id = order.partner_id.purchase_incoterm_id
+            elif not order.partner_id:
+                order.incoterm_id = False
+            return
+
