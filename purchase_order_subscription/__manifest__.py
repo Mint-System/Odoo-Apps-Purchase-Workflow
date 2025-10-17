bbd172ff59860ec853b09e3a64e441a86f8c6b01
Janik von Rotz <login@janikvonrotz.ch>
2025-10-17 14:15:07 +0200

    chore: remove coding guidelines

---
+++ purchase_order_subscription/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Purchase Order Subscription",
+    "summary": """
+        Setup recurring purchase orders.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch/",
+    "category": "Purchase",
+    "version": "16.0.1.0.0",
+    "license": "OPL-1",
+    "depends": ["purchase", "sale_subscription"],
+    "data": ["data/purchase_order_data.xml", "views/purchase_order.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
