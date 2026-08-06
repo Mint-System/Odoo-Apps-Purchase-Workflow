ca8467652866a08e8f3761f4edcea93fac86b7c4
Ulrich Kilian <uk@sdcience-and-more.de>
2026-08-06 15:01:22 +0200

    fix(purchase_requisition_send): visibilty of status bars

--- purchase_requisition_incoterm/models/purchase_order.py
+++ purchase_requisition_incoterm/models/purchase_order.py
@@ -8,12 +8,6 @@ _logger = logging.getLogger(__name__)
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    # @api.onchange("requisition_id")
-    # def _onchange_requisition_id(self):
-    #     super()._onchange_requisition_id()
-    #     if self.requisition_id and self.requisition_id.incoterm_id:
-    #         self.incoterm_id = self.requisition_id.incoterm_id
-
 
     def _apply_requisition_incoterm(self):
         for order in self:
